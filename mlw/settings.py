3a385bead8f577b03210346153b352682d72485f
Mominur Rahman <mominurrohman774@gmail.com>
2023-08-06 23:57:42 +0600

    add actor profile

--- mlw/settings.py
+++ mlw/settings.py
@@ -131,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
