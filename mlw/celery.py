cce5eda09561b85db7983e5f2240b3f0320bc4f8
ganster2 <nurmuhammad200509@gmail.com>
2023-10-03 05:36:47 -0700

    a
    sdda

--- mlw/celery.py
+++ mlw/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mlw.settings')
--- mlw/settings.py
+++ mlw/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import environ
 import os
 from pathlib import Path
+from celery import Celery
 
 
 env = environ.Env(
