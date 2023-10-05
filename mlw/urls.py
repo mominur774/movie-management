d7d18461bc011b0bd433501b632ae809f3f4838f
ganster2 <nurmuhammad200509@gmail.com>
2023-10-04 22:57:20 -0700

    aasad
    dadwdsd

--- mlw/urls.py
+++ mlw/urls.py
@@ -24,6 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movies.urls')),
     path('accounts/', include('users.urls')),
-]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
